65618197fd80e1bc5b5c42e239e85ea384a78f90
muhammettarik <takiko337@gmail.com>
2023-01-30 14:55:25 +0300

    first commit

---
+++ todo.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/tarik.can/Desktop/flaskTodoApp/todo.db"
+# create the extension
+db = SQLAlchemy()
+# initialize the app with the extension
+db.init_app(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+@app.route("/")
+def index():
+    todos = Todo.query.all()
+
+    return render_template("index.html", todos = todos)
+
+@app.route("/add", methods=["POST"])
+def addTodo():
+    title = request.form.get("title")
+    newTodo = Todo(title = title, complete = False)
+    db.session.add(newTodo)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+@app.route("/complete/<string:id>")
+def completeTodo(id):
+    todo = Todo.query.filter_by(id = id).first()
+
+    """if todo.complete == True:
+        todo.complete = False
+    else:
+        todo.complete = True"""
+
+    todo.complete = not todo.complete
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+@app.route("/delete/<string:id>")
+def deleteTodo(id):
+    todo = Todo.query.filter_by(id = id).first()
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
+
+
+
+
